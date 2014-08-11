79d406de943ae7afed1eab4cedb2718ff7bd42c0
Lawrence <jiangyi0425@gmail.com>
2014-08-12 00:06:37 +0800

    Hash

---
+++ Merge Intervals.cpp
@@ -0,0 +1,33 @@
+#include <vector>
+
+struct Interval {
+    int start;
+    int end;
+    Interval() : start(0), end(0) {}
+    Interval(int s, int e) : start(s), end(e) {}
+};
+
+static bool cmp(Interval x,Interval y){
+    if(x.start!=y.start) return x.start<y.start;
+    else return y.end<y.end;
+}
+
+vector<Interval> merge(vector<Interval> &intervals) {
+    vector<Interval> vec;
+    if(intervals.size()==0) return vec;
+    sort(intervals.begin(),intervals.end(),cmp);
+    Interval cur,cnt=intervals[0];
+    for(int i=1;i<intervals.size();i++){
+        if(cnt.end>=intervals[i].start){
+            cur.start=min(cnt.start,intervals[i].start);
+            cur.end=max(cnt.end,intervals[i].end);
+            cnt=cur;
+        }
+        else{
+            vec.push_back(cnt);
+            cnt=intervals[i];
+        }
+    }
+    vec.push_back(cnt);
+    return vec;
+}
