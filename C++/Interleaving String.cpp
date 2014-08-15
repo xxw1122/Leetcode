1302243b8b5e0dc72fe48a2c06d7f071523f54dd
Lawrence <jiangyi0425@gmail.com>
2014-08-15 19:28:58 +0800

    dp

--- C++/Interleaving String.cpp
+++ C++/Interleaving String.cpp
@@ -4,10 +4,19 @@ using namespace std;
 
 bool isInterleave(string s1, string s2, string s3) {
   int len1=s1.size(),len2=s2.size(),len3=s3.size();
-  int dp[100][100][200];
+  if(len1+len2!=len3) return false;
+  int dp[500][500];
   memset(dp,0,sizeof(dp));
-  for(int i=0;i<len1;i++){
-    for(int j=0;j<len2;j++){
+  dp[0][0]=1;
+  for(int i=0;i<=len1;i++){
+    for(int j=0;j<=len2;j++){
+        if(i>0&&s1[i-1]==s3[i+j-1]&&dp[i-1][j]){
+            dp[i][j]=1;
+        }
+        if(j>0&&s2[j-1]==s3[i+j-1]&&dp[i][j-1]){
+            dp[i][j]=1;
+        }
     }
   }
+  if(dp[len1][len2]) return true;
 }
