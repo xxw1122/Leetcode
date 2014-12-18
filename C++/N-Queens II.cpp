d88072cb18fc5a19fc0cc8827fe64c2321f43a9c
Lawrence <jiangyi0425@gmail.com>
2014-12-18 15:28:44 +0800

    Cpp

--- C++/N-Queens II.cpp
+++ C++/N-Queens II.cpp
@@ -1,29 +1,23 @@
-#include<iostream>
-
-
-void search(int cur,int &tot,int lenth,int vis[3][30],int *pos)
-    {
+class Solution {
+public:
+    void search(int cur,int &tot,int lenth,int vis[3][30]){
         if(cur==lenth) tot++;
-        else
-            {
-                for(int i=0;i<lenth;i++)
-                    {
-                        if(!vis[0][i]&&!vis[1][cur+i]&&!vis[2][cur-i+n])
-                            {
-                                pos[cur]=i;
-                                vis[0][i]=vis[1][cur+i]=vis[2][cur-i+n]=1;
-                                search(cur+1,tot,lenth,vis,pos);
-                                vis[0][i]=vis[1][cur+i]=vis[2][cur-i+n]=0;
-                            }
-                    }
+        else{
+            for(int i=0;i<lenth;i++){
+                if(!vis[0][i]&&!vis[1][cur+i]&&!vis[2][cur-i+lenth]){
+                    vis[0][i]=vis[1][cur+i]=vis[2][cur-i+lenth]=1;
+                    search(cur+1,tot,lenth,vis);
+                    vis[0][i]=vis[1][cur+i]=vis[2][cur-i+lenth]=0;
+                }
             }
+        }
     }
 
-int totalNQueens(int n)
-    {
-        int vis[3][30],pos[30];
+    int totalNQueens(int n){
+        int vis[3][30];
         memset(vis,0,sizeof(vis));
         int total=0;
-        search(0,total,n,vis,pos);
+        search(0,total,n,vis);
         return total;
     }
+};
