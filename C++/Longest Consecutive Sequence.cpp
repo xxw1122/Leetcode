e7a050925a9c54144ef6b5dd3ae31bd4545d62c6
Lawrence <jiangyi0425@gmail.com>
2014-12-19 20:56:00 +0800

    Cpp

--- C++/Longest Consecutive Sequence.cpp
+++ C++/Longest Consecutive Sequence.cpp
@@ -1,27 +1,30 @@
-#include <iostream>
-#include <algorithm>
-#include <vector>
-
-static bool cmp(int x,int y){
-    return x<y;
-}
-
-int longestConsecutive(vector<int> &num) {
-    int len=num.size();
-    if(len==0||len==1) return len;
-    sort(num.begin(),num.end(),cmp);
-    int len1=1,cnt=num[0];
-    int maxlen=len1;
-    for(int i=1;i<len;i++){
-        if(num[i]==cnt+1){
-            cnt++;
-            len1++;
-            maxlen=max(maxlen,len1);
+class Solution {
+public:
+    int longestConsecutive(vector<int> &num) {
+        unordered_set<int> st;
+        for(int i = 0; i < num.size(); i++){
+            if(st.find(num[i]) == st.end()){
+                st.insert(num[i]);
+            }
         }
-        else if(num[i]>cnt+1){
-            cnt=num[i];
-            len1=1;
+        int cnt = 1;
+        for(int i = 0; i < num.size(); i++){
+            if(st.find(num[i]) == st.end()) continue;
+            int cur = num[i] - 1;
+            int ans = 1;
+            while(st.find(cur) != st.end()){
+                st.erase(cur);
+                ans++;
+                cur--;
+            }
+            cur = num[i] + 1;
+            while(st.find(cur) != st.end()){
+                st.erase(cur);
+                ans++;
+                cur++;
+            }
+            cnt = max(cnt, ans);
         }
+        return cnt;
     }
-    return maxlen;
-}
+};
