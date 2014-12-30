10e573b8bcec677b765daddd2e714f389ae50a18
Lawrence <jiangyi0425@gmail.com>
2014-12-30 22:53:42 +0800

    Cpp

--- C++/3Sum Closest.cpp
+++ C++/3Sum Closest.cpp
@@ -1,56 +1,21 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
-
-using namespace std;
-
-int select(int x,int y,int target){
-    int a=x-target;
-    int b=y-target;
-    if(a<0) a=-a;
-    if(b<0) b=-b;
-    if(a>b) return y;
-    else return x;
-}
-
-int search(vector<int> &num,int left,int right,int cnt){
-    if(cnt>=num[right]) return num[right];
-    if(cnt<=num[left]) return num[left];
-    if(left==right) return num[left];
-    int mid;
-    while(left<right){
-        mid=(left+right)/2;
-        if(num[mid]==cnt) return num[mid];
-        else if(num[mid]>cnt){
-            if(mid>left&&num[mid-1]<=cnt){
-                return select(num[mid-1],num[mid],cnt);
-            }
-            else{
-                right=mid-1;
+class Solution {
+public:
+    int threeSumClosest(vector<int> &num, int target) {
+        int len = num.size();
+        sort(num.begin(), num.end());
+        int res = num[0] + num[1] + num[2];
+        for(int i = 0; i < len; i++){
+            int left = i + 1, right = len - 1;
+            while(left < right){
+                int s = num[i]  + num[left] + num[right];
+                if(abs(s - target) < abs(res - target)){
+                    res = s;
+                }
+                if(s == target) return s;
+                else if(s < target) left++;
+                else right--;
             }
         }
-        else if(num[mid]<cnt){
-            if(mid+1<right&&num[mid+1]>=cnt){
-                return select(num[mid+1], num[mid], cnt);
-            }
-            else{
-                left=mid+1;
-            }
-        }
+        return res;
     }
-}
-
-int threeSumClosest(vector<int> &num, int target) {
-    sort(num.begin(),num.end());
-    int len=num.size();
-    int minval=99999999;
-    for(int i=0;i<len;i++){
-        for(int j=i+1;j<len-1;j++){
-            int cnt=target-num[i]-num[j];
-            int cur=search(num,j+1,len-1,cnt);
-            int sum=cur+num[i]+num[j];
-            minval=select(minval, sum, target);
-        }
-    }
-    return minval;
-}
+};
--- C++/3Sum.cpp
+++ C++/3Sum.cpp
@@ -1,43 +1,32 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
-
-using namespace std;
-
-vector<vector<int> > threeSum(vector<int> &num){
-    sort(num.begin(),num.end());
-    vector<vector<int> >vec;
-    int len=num.size();
-    for(int i=0;i+2<len;i++){
-        if(i>0&&num[i]==num[i-1])
-                continue;
-        int j=i+1,k=len-1;
-        int cnt=-num[i];
-        while(k>j){
-            if(j>i+1&&num[j]==num[j-1]){
-                j++;
-                continue;
-            }
-            if(k<len-1&&num[k]==num[k+1]){
-                k--;
-                continue;
-            }
-            int cur=num[j]+num[k];
-            if(cur>cnt){
-                k--;
-            }
-            else if(cur<cnt){
-                j++;
-            }
-            else if(cur==cnt){
-                vector<int> vec1;
-                vec1.push_back(num[i]);
-                vec1.push_back(num[j]);
-                vec1.push_back(num[k]);
-                vec.push_back(vec1);
-                j++;
+class Solution {
+public:
+    vector<vector<int> > threeSum(vector<int> &num) {
+        vector<vector<int> >vec;
+        sort(num.begin(), num.end());
+        int len = num.size();
+        for(int i = 0; i + 2 < len; i++){
+            if(i == 0 || (i > 0 && num[i] != num[i-1])){
+                int left = i + 1, right = len - 1;
+                while(left < right){
+                    int s = num[i] + num[left] + num[right];
+                    if(s == 0){
+                        vector<int> vec1;
+                        vec1.push_back(num[i]);
+                        vec1.push_back(num[left]);
+                        vec1.push_back(num[right]);
+                        vec.push_back(vec1);
+                        left++;
+                        right--;
+                        while(left < right && num[left] == num[left-1])
+                            left++;
+                        while(left < right && num[right] == num[right+1])
+                            right--;
+                    }
+                    else if(s < 0) left++;
+                    else right--;
+                }
             }
         }
+        return vec;
     }
-    return vec;
-}
+};
