c306ae5f50d2acd95f104dd72ff921c2fa7d35c3
Lawrence <jiangyi0425@gmail.com>
2015-01-04 20:26:21 +0800

    Cpp

--- C++/Gas Station.cpp
+++ C++/Gas Station.cpp
@@ -1,38 +1,20 @@
-#include <iostream>
-#include <vector>
-
-using namespace std;
-
-int canCompleteCircuit(vector<int> &gas, vector<int> &cost) {
-    int num=gas.size();
-    int sum1=0,sum2=0;
-    vector<int> vec;
-    for(int i=0;i<num;i++){
-        sum1+=gas[i];
-        sum2+=cost[i];
-        vec.push_back((gas[i]-cost[i]));
-    }
-    if(sum1<sum2) return -1;
-    for(int i=0;i<num;i++){
-        vec.push_back(vec[i]);
-    }
-    int sum,max;
-    sum=max=-0xfffffff;
-    int left,pos=0,cnt;
-    for(int i=0;i<vec.size();i++){
-        if(sum+vec[i]<vec[i]||pos>=num){
-            sum=vec[i];
-            cnt=i;
-            pos=1;
+class Solution {
+public:
+    int canCompleteCircuit(vector<int> &gas, vector<int> &cost) {
+        int sum1 = 0, sum2 = 0;
+        for(int i = 0; i < gas.size(); i++){
+            sum1 += gas[i];
+            sum2 += cost[i];
         }
-        else{
-            pos++;
-            sum=sum+vec[i];
+        if(sum1 < sum2) return -1;
+        int diff = 0, index = 0;
+        for(int i = 0; i < gas.size(); i++){
+            if(diff + gas[i] < cost[i]){
+                diff = 0;
+                index = i + 1;
+            }
+            else diff += gas[i] - cost[i];
         }
-        if(max<sum){
-            max=sum;
-            left=cnt;
-        }
+        return index;
     }
-    return left;
-}
+};
