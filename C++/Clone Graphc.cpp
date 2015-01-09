f1beac0544cf32dfe688e49932f19c8a478e7b3f
Lawrence <jiangyi0425@gmail.com>
2015-01-09 20:03:50 +0800

    Cpp

--- C++/Clone Graphc.cpp
+++ C++/Clone Graphc.cpp
@@ -12,21 +12,19 @@ struct UndirectedGraphNode {
 };
 
 UndirectedGraphNode *cloneGraph(UndirectedGraphNode *node) {
-    if(node==NULL) return NULL;
+    if(node == NULL) return NULL;
     queue<UndirectedGraphNode*>st1;
     map<int,UndirectedGraphNode*>st2;
-    UndirectedGraphNode *root=new UndirectedGraphNode(sizeof(UndirectedGraphNode));
-    root->label=node->label;
+    UndirectedGraphNode *root = new UndirectedGraphNode(node->label);
     st2.insert(make_pair(node->label, root));
     st1.push(node);
     while(!st1.empty()){
-        int cur=st1.front()->label;
-        for(int i=0;i<st1.front()->neighbors.size();i++){
-            int cnt=(st1.front()->neighbors)[i]->label;
-            if(st2.count(cnt)==0){
+        int cur = st1.front()->label;
+        for(int i = 0; i < st1.front()->neighbors.size(); i++){
+            int cnt = (st1.front()->neighbors)[i]->label;
+            if(st2.count(cnt) == 0){
                 st1.push((st1.front()->neighbors)[i]);
-                UndirectedGraphNode* root1=new UndirectedGraphNode(sizeof(UndirectedGraphNode));
-                root1->label=cnt;
+                UndirectedGraphNode* root1 = new UndirectedGraphNode(cnt);
                 st2[cur]->neighbors.push_back(root1);
                 st2.insert(make_pair(cnt,root1));
             }
--- C++/Copy List with Random Pointer.cpp
+++ C++/Copy List with Random Pointer.cpp
@@ -10,40 +10,30 @@ struct RandomListNode {
 
 
 RandomListNode *copyRandomList(RandomListNode *head) {
-    if(head==NULL) return NULL;
-    map<int,RandomListNode*>st;
-    RandomListNode* root=new RandomListNode(sizeof(RandomListNode));
-    int flag=0;
-    RandomListNode *node=NULL;
-    RandomListNode *node1=head;
-    while(node1!=NULL){
-        if(flag==0){
-            root->label=node1->label;
-            st.insert(make_pair(root->label,root));
-            node=root;
-            flag=1;
+    if(head == NULL) return NULL;
+    map<int, RandomListNode*> mp;
+    RandomListNode *prenode = new RandomListNode(head->label);
+    RandomListNode *p = prenode, *rhead = head, *p1;
+    while(rhead != NULL){
+        RandomListNode *node = new RandomListNode(rhead->label);
+        mp[rhead->label] = node;
+        p->next = node;
+        p = p->next;
+        rhead = rhead->next;
+    }
+    p->next = NULL;
+    rhead = prenode->next;
+    p1 = head;
+    while(rhead != NULL){
+        if(p1->random == NULL){
+            rhead->random = NULL;
         }
         else{
-            RandomListNode *node3=new RandomListNode(sizeof(RandomListNode));
-            node3->label=node1->label;
-            node->next=node3;
-            node=node3;
-            st.insert(make_pair(node3->label,node3));
+            int cnt = p1->random->label;
+            rhead->random = mp[cnt];
         }
-        node1=node1->next;
+        rhead = rhead->next;
+        p1 = p1->next;
     }
-    node->next=NULL;
-    node1=head,node=root;
-    while(node1!=NULL){
-        if(node1->random==NULL){
-            node->random=NULL;
-        }
-        else{
-            int cnt=node1->random->label;
-            node->random=st[cnt];
-        }
-        node=node->next;
-        node1=node1->next;
-    }
-    return root;
+    return prenode->next;
 }
