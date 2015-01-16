a6a4cc436e73e19b3db8b3c4259ead2dcff3f2cd
Lawrence <jiangyi0425@gmail.com>
2015-01-16 20:02:57 +0800

    Cpp

--- C++/Longest Valid Parentheses.cpp
+++ C++/Longest Valid Parentheses.cpp
@@ -1,39 +1,22 @@
 #include <stack>
 #include <string>
-#include <iostream>
 
-using namespace std;
-
-int longestValidParentheses(string s) {
-    int len=s.size(),maxlen=0,cnt=0;
-    int vis[len];
-    memset(vis,0,sizeof(int)*len);
-    stack<char>st;
-    int pos=0;
-    for(int i=0;i<len;i++){
-        if(s[i]==')'&&st.empty()){
-            cnt=0;
-            memset(vis,0,sizeof(int)*len);
+class Solution {
+public:
+    int longestValidParentheses(string s) {
+        int maxlen = 0, last = -1;
+        stack<int> st;
+        for(int i = 0; i < s.size(); i ++){
+            if(s[i] == '(') st.push(i);
+            else{
+                if(st.empty()) last = i;
+                else{
+                    st.pop();
+                    if(st.empty()) maxlen = max(maxlen, i - last);
+                    else maxlen = max(maxlen, i - st.top());
+                }
+            }
         }
-        else if(s[i]==')'&&st.top()=='('){
-            st.pop();
-            pos--;
-            vis[pos]=vis[pos]+2+vis[pos+1];
-            vis[pos+1]=0;
-            maxlen=max(maxlen,vis[pos]);
-        }
-        else if(s[i]=='('){
-            pos++;
-            st.push(s[i]);
-        }
+        return maxlen;
     }
-    return maxlen;
-}
-
-int main(){
-    string str;
-    while(cin>>str){
-        int cnt=longestValidParentheses(str);
-        cout<<cnt<<endl;
-    }
-}
+};
