1bae99100c30fb86b329f7a1a81d31f611c4884e
Lawrence <jiangyi0425@gmail.com>
2015-02-13 23:30:43 +0800

    Python

---
+++ Python/Median of Two Sorted Arrays.py
@@ -0,0 +1,20 @@
+class Solution:
+    # @return a float
+    # @line20 must multiply 0.5 for return a float else it will return an int
+    def getKth(self, A, B, k):
+        lenA = len(A); lenB = len(B)
+        if lenA > lenB: return self.getKth(B, A, k)
+        if lenA == 0: return B[k - 1]
+        if k == 1: return min(A[0], B[0])
+        pa = min(k/2, lenA); pb = k - pa
+        if A[pa - 1] <= B[pb - 1]:
+            return self.getKth(A[pa:], B, pb)
+        else:
+            return self.getKth(A, B[pb:], pa)
+
+    def findMedianSortedArrays(self, A, B):
+        lenA = len(A); lenB = len(B)
+        if (lenA + lenB) % 2 == 1:
+            return self.getKth(A, B, (lenA + lenB)/2 + 1)
+        else:
+            return (self.getKth(A, B, (lenA + lenB)/2) + self.getKth(A, B, (lenA + lenB)/2 + 1)) * 0.5
