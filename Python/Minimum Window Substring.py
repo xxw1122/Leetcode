ddea55b3f480f8bb15f2c130e8330f07d8cd7f61
Lawrence <jiangyi0425@gmail.com>
2015-03-01 21:22:51 +0800

    Python

---
+++ Python/Minimum Window Substring.py
@@ -0,0 +1,30 @@
+class Solution:
+    # @return a string
+    def minWindow(self, S, T):
+        count1={}; count2={}
+        for char in T:
+            if char not in count1: count1[char]=1
+            else: count1[char]+=1
+        for char in T:
+            if char not in count2: count2[char]=1
+            else: count2[char]+=1
+        count=len(T)
+        start=0; minSize=100000; minStart=0
+        for end in range(len(S)):
+            if S[end] in count2 and count2[S[end]]>0:
+                count1[S[end]]-=1
+                if count1[S[end]]>=0:
+                    count-=1
+            if count==0:
+                while True:
+                    if S[start] in count2 and count2[S[start]]>0:
+                        if count1[S[start]]<0:
+                            count1[S[start]]+=1
+                        else:
+                            break
+                    start+=1
+                if minSize>end-start+1:
+                    minSize=end-start+1; minStart=start
+        if minSize==100000: return ''
+        else:
+            return S[minStart:minStart+minSize]
