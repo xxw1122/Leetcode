323df0b23a9233a95d06b63b310b297cf6dff977
Lawrence <jiangyi0425@gmail.com>
2015-03-03 16:11:02 +0800

    Cpp

--- C++/Linked List Cycle.cpp
+++ C++/Linked List Cycle.cpp
@@ -1,31 +1,29 @@
-#include<iostream>
-
-using namespace std;
-
-
 struct ListNode {
     int val;
     ListNode *next;
     ListNode(int x) : val(x), next(NULL) {}
 };
 
-bool hasCycle(ListNode *head) {
-    ListNode *node1,*node2;
-    node1=head,node2=head;
-    node1=node1->next,node2=(node2->next)->next;
-    if(node1==NULL||node2==NULL) return false;
-    while(node1!=node2)
-        {
-            node1=node1->next;
-            node2=node2->next;
-            if(node2==NULL) return false;
-            node2=node2->next;
-            if(node2==NULL) return false;
+class Solution {
+public:
+    /**
+     * @param head: The first node of linked list.
+     * @return: True if it has a cycle, or false
+     */
+    bool hasCycle(ListNode *head) {
+        // write your code here
+        if (head == NULL || head ->next == NULL) return false;
+        ListNode *node1 = head, *node2 = head;
+        node1 = node1->next;
+        node2 = (node2->next)->next;
+        if (node2 == NULL) return false;
+        while (node1 != node2) {
+            node1 = node1->next;
+            node2 = node2->next;
+            if (node2 == NULL) return false;
+            node2 = node2->next;
+            if (node2 == NULL) return false;
         }
-    return true;
+        return true;
     }
-
-int main()
-    {
-        return 0;
-    }
+};
