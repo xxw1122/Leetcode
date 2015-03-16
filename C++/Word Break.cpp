a24841e3adc64914b2b21e6e97f3bfc80850c569
Lawrence <jiangyi0425@gmail.com>
2015-03-16 11:42:20 +0800

    Cpp

--- C++/Word Break.cpp
+++ C++/Word Break.cpp
@@ -1,36 +1,23 @@
-#include <cstring>
-#include <string>
-#include <unordered_set>
-
-
-using namespace std;
-
-bool wordBreak(string s, unordered_set<string> &dict) {
-  int dp[1000][1000];
-  memset(dp,0,sizeof(dp));
-  int len=s.size();
-  for(int i=0;i<len;i++){
-    string str;
-    str=str+s[i];
-    if(dict.count(str)){
-      dp[i][i]=1;
+class Solution {
+public:
+    /**
+     * @param s: A string s
+     * @param dict: A dictionary of words dict
+     */
+    bool wordBreak(string s, unordered_set<string> &dict) {
+        // write your code here
+        int len = s.size();
+        vector<int> vec(len + 1);
+        vec[0] = 1;
+        for (int i = 1; i <= len; i ++) {
+            for (int j = i; j >= 1; j --) {
+                string str = s.substr(j-1, i - j + 1);
+                if (vec[j-1] == 1 && dict.find(str) != dict.end()) {
+                    vec[i] = 1;
+                    break;
+                }
+            }
+        }
+        return vec[len];
     }
-  }
-  for(int i=2;i<=len;i++){
-    for(int j=0;j+i-1<len;j++){
-      string str;
-      int r=j+i-1;
-      for(int h=j;h<=r;h++)
-        str=str+s[h];
-      if(dict.count(str)){
-        dp[j][r]=1;
-        continue;
-      }
-      for(int k=j;k<=r-1;k++){
-        dp[j][r]=(dp[j][r]|(dp[j][k]&dp[k+1][r]));
-      }
-    }
-  }
-  if(dp[0][len-1]) return true;
-  else return false;
-}
+};
