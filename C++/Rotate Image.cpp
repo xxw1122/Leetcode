682f11c827965348c345871519906aeae48cc2a2
Lawrence <jiangyi0425@gmail.com>
2015-05-01 12:21:13 +0800

    Cpp

--- C++/Rotate Image.cpp
+++ C++/Rotate Image.cpp
@@ -1,27 +1,12 @@
-#include<iostream>
-#include<vector>
+#include <vector>
 
-void rotate(vector<vector<int> > &matrix)
-    {
-        int num=matrix.size();
-        if(num==0) return;
-        int cnt;
-        for(int i=0;i<num;i++)
-            {
-                for(int j=i+1;j<num;j++)
-                    {
-                        cnt=matrix[i][j];
-                        matrix[i][j]=matrix[j][i];
-                        matrix[j][i]=cnt;
-                    }
-            }
-        for(int i=0;i<num/2;i++)
-            {
-                for(int j=0;j<num;j++)
-                    {
-                        cnt=matrix[j][i];
-                        matrix[j][i]=matrix[j][num-1-i];
-                        matrix[j][num-1-i]=cnt;
-                    }
-            }
+class Solution {
+public:
+    /**
+     * @param matrix: A list of lists of integers
+     * @return: Void
+     */
+    void rotate(vector<vector<int> > &matrix) {
+        // write your code here
     }
+};
--- C++/Word Ladder.cpp
+++ C++/Word Ladder.cpp
@@ -1,31 +1,43 @@
 #include <unordered_set>
-#include <string>
 #include <queue>
+#include <pair>
 #include <map>
+#include <string>
 
-using namespace std;
-
-int ladderLength(string start, string end, unordered_set<string> &dict){
-  unordered_set<string>st;
-  queue<pair<string,int> >que;
-  st.insert(start);
-  que.push(make_pair(start,1));
-  while(!que.empty()){
-    string str=que.front().first,s;
-    int length=que.front().second;
-    if(str==end) return length;
-    length++;
-    que.pop();
-    for(int i=0;i<str.size();i++){
-      for(int j=0;j<26;j++){
-        s=str;
-        s[i]='a'+j;
-        if(st.count(s)==0&&dict.count(s)>0){
-          st.insert(s);
-          que.push(make_pair(s,length));
+class Solution {
+public:
+    /**
+      * @param start, a string
+      * @param end, a string
+      * @param dict, a set of string
+      * @return an integer
+      */
+    int ladderLength(string start, string end, unordered_set<string> &dict) {
+        // write your code here
+        if (start == end) return 1;
+        queue<pair<string, int> >que;
+        unordered_set<string> st;
+        que.push(make_pair(start, 1));
+        st.insert(start);
+        if (dict.find(start) != dict.end()) dict.erase(start);
+        while (!que.empty()) {
+            string str = que.front().first, s;
+            int len = que.front().second;
+            if (str == end) return len;
+            len ++;
+            que.pop();
+            for (int i = 0; i < str.size(); i ++) {
+                for (int j = 0; j < 26; j ++) {
+                    s = str;
+                    s[i] = j + 'a';
+                    if (dict.find(s) != dict.end() && st.find(s) == st.end()) {
+                        st.insert(s);
+                        dict.erase(s);
+                        que.push(make_pair(s, len));
+                    }
+                }
+            }
         }
-      }
+        return 0;
     }
-  }
-  return 0;
-}
+};
