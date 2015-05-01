c6c09258a24f850c6eb97d02f1bcaad7bb6c8afc
Lawrence <jiangyi0425@gmail.com>
2015-05-01 21:29:44 +0800

    Cpp

--- C++/Sort List.cpp
+++ C++/Sort List.cpp
@@ -1,54 +1,62 @@
-#include<iostream>
-
-using namespaces std;
-
-struct ListNode {
+/**
+ * Definition of ListNode
+ */
+class ListNode {
+public:
     int val;
     ListNode *next;
-    ListNode(int x) : val(x), next(NULL) {}
+    ListNode(int val) {
+        this->val = val;
+        this->next = NULL;
+    }
+};
+
+class Solution {
+public:
+    /**
+     * @param head: The first node of linked list.
+     * @return: You should return the head of the sorted linked list,
+     using constant space complexity.
+    */
+    ListNode *merge(ListNode *head1, ListNode *head2) {
+        if (head1 == NULL) return head2;
+        if (head2 == NULL) return head1;
+        ListNode *pre = new ListNode(-1), *cnt = pre;
+        while (head1 != NULL && head2 != NULL) {
+            if(head1->val < head2->val) {
+                cnt->next = head1;
+                head1 = head1->next;
+            } else {
+                cnt->next = head2;
+                head2 = head2->next;
+            }
+            cnt = cnt->next;
+        }
+        if (head1 != NULL) {
+            cnt->next = head1;
+        } else if (head2 != NULL){
+            cnt->next = head2;
+        } else {
+            cnt->next = NULL;
+        }
+        cnt = pre->next;
+        delete pre;
+        return cnt;
+    }
+    ListNode *sortList(ListNode *head) {
+        // write your code here
+        if (head == NULL || head->next == NULL) return head;
+        ListNode *slow = head, *fast = head, *cnt = head;
+        while (fast->next != NULL && fast->next->next != NULL) {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        fast = slow->next;
+        slow->next = NULL;
+        slow = head;
+        return merge(sortList(slow), sortList(fast));
+    }
 };
 
 
-ListNode *mergelist(ListNode *head1,ListNode *head2)
-{
-    ListNode *temp;
-    if(head1==NULL) return head2;
-    if(head2==NULL) return head1;
-    if(head1->val<head2->val)
-        {
-            temp=head1;
-            head1=head1->next;
-        }
-    else
-        {
-            temp=head2;
-            head2=head2->next;
-        }
-    temp->next=mergelist(head1, head2);
-    return temp;
-}
 
-ListNode *sortList(ListNode *head)
-{
-    if(head==NULL) return NULL;
-    ListNode *r_head=head;
-    ListNode *head1=head;
-    ListNode *head2=head;
-    //if(head->next) return head;
-    while(head2->next!=NULL&&head2->next->next!=NULL)
-        {
-            head1=head1->next;
-            head2=head2->next->next;
-        }
-    if(head1->next==NULL) return r_head;
-    head2=head1->next;
-    head1->next=NULL;
-    head1=head;
-    r_head=mergelist(sortList(head1),sortList(head2));
-    return r_head;
-}
-
-int main()
-    {
-        return 0;
-    }
