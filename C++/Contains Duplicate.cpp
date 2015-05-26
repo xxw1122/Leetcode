5a650ed10e662c36605a227ac2421d2d943f6e0a
Lawrence <jiangyi0425@gmail.com>
2015-05-26 23:25:30 +0800

    Cpp

---
+++ C++/Contains Duplicate.cpp
@@ -0,0 +1,16 @@
+#include <vector>
+#include <set>
+
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        set<int> st;
+        for (int i = 0; i < nums.size(); i ++) {
+            if (st.find(nums[i]) != st.end()) return true;
+            else {
+                st.insert(nums[i]);
+            }
+        }
+        return false;
+    }
+};
