ae4bcdac36cf28a8d3a136a7291d753b0b626133
Lawrence <jiangyi0425@gmail.com>
2015-05-31 18:26:24 +0800

    Cpp

--- C++/LRU Cache.cpp
+++ C++/LRU Cache.cpp
@@ -1,51 +1,88 @@
-struct CacheNode
-{
-    int key;
-    int value;
-    CacheNode(int k, int v):key(k), value(v){}
-};
+#include <map>
 
 class LRUCache{
 public:
+    class ListNode {
+    public:
+        int val;
+        ListNode *next, *prev;
+        ListNode(int val) {
+            this->val = val;
+            this->next = NULL;
+            this->prev = NULL;
+        }
+    };
+    map<int, int> hash1;
+    map<int, ListNode*> hash2;
+    int cap, cur;
+    ListNode *head, *tail;
+    // @param capacity, an integer
     LRUCache(int capacity) {
-        size = capacity;
+        // write your code here
+        cap = capacity;
+        cur = 0;
+        head = new ListNode(-1);
+        tail = new ListNode(-1);
+        head->next = tail;
+        tail->prev = head;
     }
-
+    void move(ListNode *cnt) {
+        ListNode *cnt_next = cnt->next, *cnt_prev = cnt->prev;
+        cnt_prev->next = cnt_next;
+        cnt_next->prev = cnt_prev;
+        ListNode *head_next = head->next;
+        head->next = cnt;
+        cnt->prev = head;
+        cnt->next = head_next;
+        head_next->prev = cnt;
+    }
+    void add(ListNode *cnt) {
+        ListNode *head_next = head->next;
+        head->next = cnt;
+        cnt->prev = head;
+        cnt->next = head_next;
+        head_next->prev = cnt;
+    }
+    void solve(int key, int value) {
+        int k = tail->prev->val;
+        hash1.erase(k);
+        hash2.erase(k);
+        hash1.insert(make_pair(key, value));
+        ListNode *p = tail->prev;
+        p->val = key;
+        move(p);
+        hash2.insert(make_pair(key, p));
+    }
+    // @return an integer
     int get(int key) {
-        if(cacheMap.find(key) == cacheMap.end())
+        // write your code here
+        if (hash1.find(key) == hash1.end()) {
             return -1;
-        else
-        {
-            //把当前访问的节点移到链表头部,并且更新map中该节点的地址
-            cacheList.splice(cacheList.begin(), cacheList, cacheMap[key]);
-            cacheMap[key] = cacheList.begin();
-            return cacheMap[key]->value;
+        } else {
+            ListNode *res = hash2[key];
+            move(res);
+            return hash1[key];
         }
-
     }
 
+    // @param key, an integer
+    // @param value, an integer
+    // @return nothing
     void set(int key, int value) {
-        if(cacheMap.find(key) == cacheMap.end())
-        {
-            if(cacheList.size() == size)
-            {//删除链表尾部节点（最少访问的节点）
-                cacheMap.erase(cacheList.back().key);
-                cacheList.pop_back();
+        // write your code here
+        if (hash1.find(key) != hash1.end()) {
+            hash1[key] = value;
+            get(key);
+        } else {
+            if (cur < cap) {
+                ListNode *cnt = new ListNode(key);
+                hash1.insert(make_pair(key, value));
+                hash2.insert(make_pair(key, cnt));
+                add(cnt);
+                cur ++;
+            } else {
+                solve(key, value);
             }
-            //插入新节点到链表头部,并且更新map中增加该节点
-            cacheList.push_front(CacheNode(key, value));
-            cacheMap[key] = cacheList.begin();
         }
-        else
-        {//更新节点的值，把当前访问的节点移到链表头部,并且更新map中该节点的地址
-            cacheMap[key]->value = value;
-            cacheList.splice(cacheList.begin(), cacheList, cacheMap[key]);
-            cacheMap[key] = cacheList.begin();
-        }
-
     }
-private:
-    list<CacheNode> cacheList;
-    unordered_map<int, list<CacheNode>::iterator>cacheMap;
-    int size;
 };
--- C++/Roman to Integer.cpp
+++ C++/Roman to Integer.cpp
@@ -1,38 +1,18 @@
-#include<iostream>
-#include<string>
+#include <string>
 
-using namespace std;
-
-int romanToInt(string s)
-    {
-        int sum=0,len=s.size();
-        for(int i=0;i<len;i++)
-            {
-                if(s[i]=='I')
-                    {
-                        if(i<len-1&&(s[i+1]=='V'||s[i+1]=='X')) sum=sum-1;
-                        else sum++;
-                    }
-                else if(s[i]=='V') sum=sum+5;
-                else if(s[i]=='X')
-                    {
-                        if(i<len-1&&(s[i+1]=='L'||s[i+1]=='C'))
-                            {
-                                sum=sum-10;
-                            }
-                        else sum=sum+10;
-                    }
-                else if(s[i]=='L') sum=sum+50;
-                else if(s[i]=='C')
-                    {
-                        if(i<len-1&&(s[i+1]=='D'||s[i+1]=='M'))
-                            {
-                                sum=sum-100;
-                            }
-                        else sum=sum+100;
-                    }
-                else if(s[i]=='D') sum=sum+500;
-                else if(s[i]=='M') sum=sum+1000;
-            }
-        return sum;
+class Solution {
+public:
+    int romanToInt(string &s) {
+        int map[26];
+        map['I'-'A'] = 1; map['V'-'A'] = 5; map['X'-'A'] = 10; map['L'-'A'] = 50;
+        map['C'-'A'] = 100; map['D'-'A'] = 500; map['M'-'A'] = 1000;
+        int res = 0, n = s.size();
+        s.push_back(s[n-1]);
+        for(int i = 0; i < n; i++) {
+            if(map[s[i]-'A'] >= map[s[i+1]-'A'])
+                res += map[s[i]-'A'];
+            else res -= map[s[i]-'A'];
+        }
+        return res;
     }
+};
