b065ad35d36472969c06138dcbe4d7379fb51f4a
Lawrence <jiangyi0425@gmail.com>
2015-06-07 15:54:00 +0800

    Cpp

---
+++ C++/Best Time to Buy and Sell Stock IV.cpp
@@ -0,0 +1,33 @@
+#include <vector>
+
+class Solution {
+public:
+    /**
+     * @param k: An integer
+     * @param prices: Given an integer array
+     * @return: Maximum profit
+     */
+    int maxProfit(int k, vector<int> &prices) {
+        if (prices.empty()) return 0;
+        if (k >= prices.size()) return solveMaxProfit(prices);
+        int g[k + 1] = {0};
+        int l[k + 1] = {0};
+        for (int i = 0; i < prices.size() - 1; ++i) {
+            int diff = prices[i + 1] - prices[i];
+            for (int j = k; j >= 1; --j) {
+                l[j] = max(g[j - 1] + max(diff, 0), l[j] + diff);
+                g[j] = max(g[j], l[j]);
+            }
+        }
+        return g[k];
+    }
+    int solveMaxProfit(vector<int> &prices) {
+        int res = 0;
+        for (int i = 1; i < prices.size(); ++i) {
+            if (prices[i] - prices[i - 1] > 0) {
+                res += prices[i] - prices[i - 1];
+            }
+        }
+        return res;
+    }
+};
