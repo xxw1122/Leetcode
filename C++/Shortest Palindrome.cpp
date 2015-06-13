8b4adeb55794e349914976e341f15f15780c933e
Lawrence <jiangyi0425@gmail.com>
2015-06-14 00:00:38 +0800

    Cpp

---
+++ C++/Shortest Palindrome.cpp
@@ -0,0 +1,15 @@
+#include <string>
+
+class Solution {
+public:
+    string shortestPalindrome(string s) {
+        string s2 = s;
+        reverse(s2.begin(), s2.end());
+        int len = s.length();
+        for (int i = len; i >= 0; i --) {
+            if (s.substr(0, i) == s2.substr(len - i)) {
+                return s2.substr(0, len - i) + s;
+            }
+        }
+    }
+};
