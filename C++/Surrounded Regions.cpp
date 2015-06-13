e8afbe86d0fefeb43271022fd5f97ee8a0b37b9c
Lawrence <jiangyi0425@gmail.com>
2015-06-13 21:36:21 +0800

    Cpp

--- C++/Surrounded Regions.cpp
+++ C++/Surrounded Regions.cpp
@@ -1,48 +1,46 @@
 #include <vector>
+#include <queue>
 
-void judge(vector<vector<char> >&board,int **vis,int x,int y,int& tag){
-    int len=board.size();
-    if(x<0||y<0||x>=len||y>=board[x].size()||vis[x][y]==1||board[x][y]=='X') return;
-    vis[x][y]=1;
-    if(x==0||y==0||x==len-1||y==board[x].size()-1) tag=1;
-    judge(board,vis,x+1,y,tag);
-    judge(board,vis,x,y+1,tag);
-    judge(board,vis,x-1,y,tag);
-    judge(board,vis,x,y-1,tag);
-    return;
-}
-
-void flip(vector<vector<char> >&board,int x,int y){
-    int len=board.size();
-    if(x<0||y<0||x>=len||y>=board[x].size()||board[x][y]=='X') return;
-    board[x][y]='X';
-    flip(board,x+1,y);
-    flip(board,x,y+1);
-    flip(board,x-1,y);
-    flip(board,x,y-1);
-}
-
-void solve(vector<vector<char> > &board) {
-    int len=board.size();
-    int **vis=new int*[len];
-    for(int i=0;i<len;i++){
-        int cnt=board[i].size();
-        vis[i]=new int[cnt];
-    }
-    for(int i=0;i<len;i++){
-        for(int j=0;j<board[i].size();j++){
-            vis[i][j]=0;
-        }
-    }
-    for(int i=0;i<len;i++){
-        for(int j=0;j<board[i].size();j++){
-            if(vis[i][j]==0&&board[i][j]=='O'){
-                int tag=0;
-                judge(board, vis, i, j, tag);
-                if(tag==0){
-                    flip(board,i,j);
+class Solution {
+public:
+    void bfs(vector<vector<char> > &board, int x, int y) {
+        int lenx = board.size(), leny = board[0].size();
+        int dx[] = {-1, 1, 0, 0}, dy[] = {0, 0, 1, -1};
+        queue<pair<int, int> > que;
+        board[x][y] = '1';
+        que.push(make_pair(x, y));
+        while (!que.empty()) {
+            int x1 = que.front().first, y1 = que.front().second;
+            que.pop();
+            for (int i = 0; i < 4; i ++) {
+                int x2 = x1 + dx[i], y2 = y1 + dy[i];
+                if (x2 >= 0 && x2 < lenx && y2 >= 0 && y2 < leny && board[x2][y2] == 'O') {
+                    board[x2][y2] = '1';
+                    que.push(make_pair(x2, y2));
                 }
             }
         }
     }
-}
+    void solve(vector<vector<char> >& board) {
+        if (board.empty() || board[0].empty()) return;
+        int lenx = board.size(), leny = board[0].size();
+        for (int i = 0; i < lenx; i ++) {
+            if (board[i][0] == 'O') bfs(board, i, 0);
+            if (board[i][leny - 1] == 'O')  bfs(board, i, leny - 1);
+        }
+        for (int i = 0; i < leny; i ++) {
+            if (board[0][i] == 'O') bfs(board, 0, i);
+            if (board[lenx - 1][i] == 'O') bfs(board, lenx - 1, i);
+        }
+        for (int i = 0; i < lenx; i ++) {
+            for (int j = 0; j < leny; j ++) {
+                if (board[i][j] == 'O') board[i][j] = 'X';
+            }
+        }
+        for (int i = 0; i < lenx; i ++) {
+            for (int j = 0; j < leny; j ++) {
+                if (board[i][j] == '1') board[i][j] = 'O';
+            }
+        }
+    }
+};
