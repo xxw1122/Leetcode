c8ae694da196ef2df54f31a8ec3a58fb72d935c5
Lawrence <jiangyi0425@gmail.com>
2015-06-13 15:13:29 +0800

    Cpp

--- C++/Maximal Rectangle.cpp
+++ C++/Maximal Rectangle.cpp
@@ -1,34 +1,40 @@
 #include <vector>
 
-int maximalRectangle(vector<vector<char> > &matrix) {
-    int len1=matrix.size();
-    if(len1==0) return 0;
-    int len2=matrix[0].size();
-    const int maxn=500;
-    int up[maxn][maxn],left[maxn][maxn],right[maxn][maxn];
-    int ans=0;
-    for(int i=0;i<len1;i++){
-        int lo=-1,ro=len2;
-        for(int j=0;j<len2;j++){
-            if(matrix[i][j]=='0'){
-                up[i][j]=left[i][j]=0;
-                lo=j;
+class Solution {
+public:
+    int maximalRectangle(vector<vector<char> >& matrix) {
+        if (matrix.empty()) return 0;
+        const int m = matrix.size();
+        const int n = matrix[0].size();
+        int left[n], right[n], height[n];
+        fill_n(left,n,0); fill_n(right,n,n); fill_n(height,n,0);
+        int maxA = 0;
+        for(int i = 0; i < m; i ++) {
+            int cur_left = 0, cur_right = n;
+            for(int j = 0; j < n; j ++) { // compute height (can do this from either side)
+                if(matrix[i][j] == '1') height[j] ++;
+                else height[j] = 0;
             }
-            else{
-                up[i][j]=i==0?1:up[i-1][j]+1;
-                left[i][j]=i==0?lo+1:max(left[i-1][j],lo+1);
+            for(int j = 0; j < n; j ++) { // compute left (from left to right)
+                if(matrix[i][j] == '1') left[j] = max(left[j], cur_left);
+                else {
+                    left[j] = 0;
+                    cur_left = j+1;
+                }
+            }
+            // compute right (from right to left)
+            for(int j = n-1; j >= 0; j --) {
+                if(matrix[i][j] == '1') right[j] = min(right[j], cur_right);
+                else {
+                    right[j] = n;
+                    cur_right = j;
+                }
+            }
+            // compute the area of rectangle (can do this from either side)
+            for(int j = 0; j < n; j ++) {
+                maxA = max(maxA, (right[j] - left[j]) * height[j]);
             }
         }
-        for(int j=len2-1;j>=0;j--){
-            if(matrix[i][j]=='0'){
-                right[i][j]=len2;
-                ro=j;
-            }
-            else{
-                right[i][j]=i==0?ro-1:min(right[i-1][j],ro-1);
-                ans=max(ans,up[i][j]*(right[i][j]-left[i][j]+1));
-            }
-        }
+        return maxA;
     }
-    return ans;
-}
+};
