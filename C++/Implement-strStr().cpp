e057d830a5c89e261f04ad6d4a661ef2698d73d3
Lawrence <jiangyi0425@gmail.com>
2015-07-22 00:12:59 +0800

    Cpp

--- C++/Implement-strStr().cpp
+++ C++/Implement-strStr().cpp
@@ -1,6 +1,14 @@
 class Solution {
 public:
     int strStr(string haystack, string needle) {
-        
+    	int len1 = haystack.size(), len2 = needle.size();
+        for (int i = 0; i + len2 - 1 < len1; i ++) {
+        	int j = 0;
+        	for (j = 0; j < len2; j ++) {
+        		if (needle[j] != haystack[i + j]) break;
+        	}
+        	if (j == len2) return i;
+        }
+        return -1;
     }
 };
--- C++/Permutations-II.cpp
+++ C++/Permutations-II.cpp
@@ -2,17 +2,17 @@ class Solution {
 public:
     bool Next_Permutation(vector<int> &nums) {
         if (nums.empty() || nums.size() <= 1) return false;
-        int pa = nums.size() - 1, pb = pa - 1, pc = pa;
-        while (pb >= 0 && nums[pb] >= nums[pa]) {
+        int pb = nums.size() - 1, pa = pb - 1, pc = pb;
+        while (pa >= 0 && nums[pa] >= nums[pb]) {
             pb --;
             pa --;
         }
-        if (pb < 0) return false;
-        while (pc >= 0 && nums[pc] <= nums[pb]) {
+        if (pa < 0) return false;
+        while (pc >= 0 && nums[pc] <= nums[pa]) {
             pc --;
         }
-        swap(nums[pb], nums[pc]);
-        int left = pa, right = nums.size() - 1;
+        swap(nums[pa], nums[pc]);
+        int left = pb, right = nums.size() - 1;
         while (left < right) {
             swap(nums[left], nums[right]);
             left ++;
@@ -20,7 +20,7 @@ public:
         }
         return true;
     }
-    vector<vector<int> > permute(vector<int>& nums) {
+    vector<vector<int> > permuteUnique(vector<int>& nums) {
         sort(nums.begin(), nums.end());
         vector<vector<int> > res;
         res.push_back(nums);
