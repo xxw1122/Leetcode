365846af35a65a90483de99b2f9f5d308e3b6d98
Lawrence <jiangyi0425@gmail.com>
2015-07-27 19:40:39 +0800

    Cpp

--- Python/pchuli.sh
+++ Python/pchuli.sh
@@ -1,4 +1,4 @@
-ls /Users/lawrence/Documents/Coding/Interview/Leetcode/Python/*.py | while read myfile
+ls /Users/lawrence/Documents/Coding/Interview/Leetcode/C++/*.cpp | while read myfile
 do
    new=`echo "$myfile" | sed 's/\ /-/g'`
    echo $new
