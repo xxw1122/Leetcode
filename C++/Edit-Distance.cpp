32f15dd4f04638281e568ef32fb330c58c015f4b
Lawrence <jiangyi0425@gmail.com>
2015-07-28 23:30:49 +0800

    Cpp

--- C++/Edit-Distance.cpp
+++ C++/Edit-Distance.cpp
@@ -1,37 +1,23 @@
-#include <iostream>
-#include <string>
-
-int dp(int len1,int len2,string str1,string str2,int **d){
-    if(d[len1][len2]!=-1) return d[len1][len2];
-    if(len1==0||len2==0){
-        int cnt=(len1-len2);
-        if(cnt<0) cnt=-cnt;
-        return d[len1][len2]=cnt;
-    }
-    if(str1[len1-1]==str2[len2-1]) d[len1][len2]=dp(len1-1,len2-1,str1,str2,d);
-    else{
-        d[len1][len2]=min(dp(len1-1,len2-1,str1,str2,d)+1,min(dp(len1-1,len2,str1,str2,d)+1,dp(len1,len2-1,str1,str2,d)+1));
-    }
-    return d[len1][len2];
-}
-
-int minDistance(string word1, string word2) {
-    int len1=word1.size(),len2=word2.size();
-    if(len1==0) return len2-len1;
-    if(len2==0) return len1-len2;
-    int **d=new int *[len1+1];
-    for(int i=0;i<len1+1;i++){
-        d[i]=new int[len2+1];
-    }
-    for(int i=0;i<len1+1;i++){
-        for(int j=0;j<len2+1;j++){
-            d[i][j]=-1;
+class Solution {
+public:
+    int minDistance(string word1, string word2) {
+        int len1 = word1.size(), len2 = word2.size();
+        vector<vector<int> > dp(len1 + 1, vector<int>(len2 + 1, 0));
+        for (int i = 0; i <= len1; i ++) {
+            dp[i][0] = i;
         }
+        for (int i = 0; i <= len2; i ++) {
+            dp[0][i] = i;
+        }
+        for (int i = 1; i <= len1; i ++) {
+            for (int j = 1; j <= len2; j ++) {
+                if (word1[i - 1] == word2[j - 1]) {
+                    dp[i][j] = dp[i - 1][j - 1];
+                } else {
+                    dp[i][j] = min(dp[i - 1][j], min(dp[i][j - 1], dp[i - 1][j - 1])) + 1;
+                }
+            }
+        }
+        return dp[len1][len2];
     }
-    int len=dp(len1,len2,word1,word2,d);
-    for(int i=0;i<len1+1;i++){
-        delete []d[i];
-    }
-    delete []p;
-    return len;
-}
+};
--- C++/Substring-with-Concatenation-of-All-Words.cpp
+++ C++/Substring-with-Concatenation-of-All-Words.cpp
@@ -3,8 +3,29 @@ public:
     vector<int> findSubstring(string s, vector<string>& words) {
         vector<int> res;
         if (words.empty()) return res;
-        int lenw = words.size(), lens = s.size(), len = word[0];
-        for (int i = 0; i + len * lenw - 1 < lens; i ++) {
+        int lenw = words.size(), lens = s.size(), len = words[0].size();
+        unordered_map<string, int> hash;
+        for (int i = 0; i < lenw; i ++) {
+        	if (hash.find(words[i]) != hash.end()) hash[words[i]] ++;
+        	else hash.insert(make_pair(words[i], 1));
         }
+        for (int i = 0; i + len * lenw - 1 < lens; i ++) {
+        	unordered_map<string, int> cur(hash);
+        	int sign = 1;
+        	for (int j = i; j < i + len * lenw; j += len) {
+        		string str = s.substr(j, len);
+        		if (cur.find(str) == cur.end()) {
+        			sign = 0;
+        			break;
+        		} else {
+        			cur[str] --;
+        			if (cur[str] == 0) {
+        				cur.erase(str);
+        			}
+        		}
+        	}
+        	if (sign == 1 && cur.empty()) res.push_back(i);
+        }
+        return res;
     }
 };
