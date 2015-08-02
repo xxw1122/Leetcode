3110a8cf2409e2cc8781d11a62df3b9d99223e1e
Lawrence <jiangyi0425@gmail.com>
2015-08-02 23:57:28 +0800

    Cpp

--- C++/Best-Time-to-Buy-and-Sell-Stock-III.cpp
+++ C++/Best-Time-to-Buy-and-Sell-Stock-III.cpp
@@ -1,6 +1,24 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        
+        int len = prices.size();
+        if (len <= 1) return 0;
+        vector<int> leftp(len, 0);
+        vector<int> rightp(len, 0);
+        int cur = prices[0];
+        for (int i = 1; i < len; i ++) {
+            leftp[i] = max(prices[i] - cur, leftp[i - 1]);
+            cur = min(prices[i], cur);
+        }
+        cur = prices[len - 1];
+        for (int i = len - 2; i >= 0; i --) {
+            rightp[i] = max(cur - prices[i], rightp[i + 1]);
+            cur = max(prices[i], cur);
+        }
+        int res = 0;
+        for (int i = 0; i < len; i ++) {
+            res = max(res, leftp[i] + rightp[i]);
+        }
+        return res;
     }
 };
