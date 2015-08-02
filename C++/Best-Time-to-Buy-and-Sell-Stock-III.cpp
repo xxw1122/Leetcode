31444f09f87be0c71e0b5fa06eb561c462a4d586
Lawrence <jiangyi0425@gmail.com>
2015-08-02 23:46:48 +0800

    Cpp

--- C++/Best-Time-to-Buy-and-Sell-Stock-III.cpp
+++ C++/Best-Time-to-Buy-and-Sell-Stock-III.cpp
@@ -1,38 +1,6 @@
-#include <vector>
-
-int maxProfit(vector<int> &prices) {
-    int len=prices.size();
-    if(len<=1) return 0;
-    int *dp1=new int[len];
-    dp1[0]=0;
-    int maxv=prices[0],minv=prices[0],maxp=0,maxnum=0;
-    for(int i=0;i<len;i++){
-        if(prices[i]>maxv){
-            maxp=max(maxp,prices[i]-minv);
-        }
-        dp1[i]=maxp;
-        if(prices[i]<minv){
-            minv=prices[i];
-            maxv=minv;
-        }
-        maxnum=max(maxnum,maxp);
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        
     }
-    int *dp2=new int[len];
-    dp2[len-1]=0;
-    maxv=prices[len-1],minv=prices[len-1],maxp=0;
-    for(int i=len-1;i>=0;i--){
-        if(prices[i]<minv){
-            maxp=max(maxp,maxv-prices[i]);
-        }
-        dp2[i]=maxp;
-        if(prices[i]>maxv){
-            maxv=prices[i];
-            minv=maxv;
-        }
-    }
-    for(int i=0;i<len;i++){
-        int cnt=dp1[i]+dp2[i];
-        maxnum=max(maxnum,cnt);
-    }
-    return maxnum;
-}
+};
--- C++/Product-of-Array-Except-Self.cpp
+++ C++/Product-of-Array-Except-Self.cpp
@@ -3,15 +3,13 @@ public:
     vector<int> productExceptSelf(vector<int>& nums) {
         int len = nums.size();
         vector<int> res(len, 1);
-        int cur = 1;
-        for (int i = 0; i < nums.size(); i ++) {
-        	res[i] = cur;
-        	cur *= nums[i];
+        for (int i = 1; i < len; i ++) {
+            res[i] = res[i - 1] * nums[i - 1];
         }
-        cur = 1;
-        for (int i = len - 1; i >= 0; i--) {
-        	res[i] = res[i] * cur;
-        	cur = cur * nums[i];
+        int cur = 1;
+        for (int i = len - 2; i >= 0; i --) {
+            cur = cur * nums[i + 1];
+            res[i] = res[i] * cur;
         }
         return res;
     }
