f428da51a4906490c51d8edc59ba76055a9fe8e3
Lawrence <jiangyi0425@gmail.com>
2015-08-05 21:34:23 +0800

    Python

--- Python/Reverse-Words-in-a-String.py
+++ Python/Reverse-Words-in-a-String.py
@@ -1,12 +1,14 @@
-#!/usr/bin/env python
-
-def reverseWords(self, s):
-    seq = s.split()
-    seq.reverse()
-    cnt = ''
-    if len(seq) == 0:
+class Solution:
+    # @param s : A string
+    # @return : A string
+    def reverseWords(self, s):
+        seq = s.split()
+        seq.reverse()
+        cnt = ''
+        if len(seq) == 0:
+            return cnt
+        for i in range(len(seq) - 1):
+            cnt += seq[i]
+            cnt += ' '
+        cnt += seq[len(seq) - 1]
         return cnt
-    for i in range(len(seq) - 1):
-        cnt += seq[i]
-        cnt += ' '
-    cnt += seq[len(seq) - 1]
