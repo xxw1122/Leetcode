f03e5450cfeffd5ff9a51de4f6e0b9be90405b22
Lawrence <jiangyi0425@gmail.com>
2015-08-05 23:35:59 +0800

    Cpp

--- C++/Recover-Binary-Search-Tree.cpp
+++ C++/Recover-Binary-Search-Tree.cpp
@@ -1,51 +1,36 @@
-#include <vector>
-
-using namespace std;
-
-struct TreeNode {
-    int val;
-    TreeNode *left;
-    TreeNode *right;
-    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* firstElement;
+    TreeNode* secondElement;
+    TreeNode* prevElement;
+    void recoverTree(TreeNode* root) {
+        firstElement = NULL;
+        secondElement = NULL;
+        prevElement = new TreeNode(INT_MIN);
+        traverse(root);
+        int temp = firstElement->val;
+        firstElement->val = secondElement->val;
+        secondElement->val = temp;
+    }
+    void traverse(TreeNode* root) {
+        if (root == NULL) return;
+        traverse(root->left);
+        if (firstElement == NULL && prevElement->val >= root->val) {
+            firstElement = prevElement;
+        }
+        if (firstElement != NULL && prevElement->val >= root->val) {
+            secondElement = root;
+        }
+        prevElement = root;
+        traverse(root->right);
+    }
 };
-
-void inorder(TreeNode* root,vector<int>&vec){
-    if(root==NULL) return;
-    inorder(root->left, vec);
-    vec.push_back(root->val);
-    inorder(root->right, vec);
-    return;
-}
-
-void swapvalue(TreeNode *root,int pos1,int val1,int pos2,int val2,int &pos){
-    if(root->left!=NULL) swapvalue(root->left, pos1, val1, pos2, val2, pos);
-    if(pos==pos1) root->val=val2;
-    if(pos==pos2) root->val=val1;
-    pos++;
-    if(root->right!=NULL) swapvalue(root->right,pos1,val1,pos2,val2,pos);
-}
-
-void recoverTree(TreeNode *root) {
-    vector<int>vec;
-    inorder(root, vec);
-    int len=vec.size();
-    int pos1,pos2;
-    int val1,val2;
-    for(int i=0;i<len;i++){
-        if(i+1<len&&vec[i+1]<vec[i]){
-            pos1=i;
-            val1=vec[pos1];
-            break;
-        }
-    }
-    for(int i=len-1;i>=0;i--){
-        if(i-1>=0&&vec[i]<vec[i-1]){
-            pos2=i;
-            val2=vec[pos2];
-            break;
-        }
-    }
-    int cnt=0;
-    swapvalue(root, pos1, val1, pos2, val2, cnt);
-}
-
--- C++/Reverse-Words-in-a-String.cpp
+++ C++/Reverse-Words-in-a-String.cpp
@@ -1,12 +1,39 @@
 class Solution {
 public:
-    void reverse(string &s, int left, int right) {
-        while (left < right) {
-            swap(s[left], s[right]);
-            left ++;
-            right --;
+    int format(string& s) {
+        int n = s.size();
+        int count = 0;
+        for (int i = 0; i < n; ++i) {
+            if (s[i] != ' ' || (i > 0 && s[i-1] != ' ')) {
+                s[count++] = s[i];
+            }
         }
+        // handle the case where after last word, all were spaces
+        if (s[count-1] == ' ') count--;
+        s.resize(count);
+        return count;
     }
+
     void reverseWords(string &s) {
+        int n = format(s); // remove all trailing and preceding spaces
+        if (n == 0) return;
+        // reverse the whole string
+        for (int i = 0; i < n/2; ++i) {
+            swap(s[i], s[n-i-1]);
+        }
+        int i = 0;
+        // reverse each word
+        while(i < n) {
+            int j = i;
+            size_t end = s.find_first_of(' ',i); 
+            i = (end == string::npos) ? n : end;
+            int e = i-1;
+            while(j < e) {
+                swap(s[j],s[e]);
+                j++;
+                e--;
+            }
+            i++;
+        }
     }
 };
