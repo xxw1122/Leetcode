133cd216ea57d78d4fc462be1bdcddb40caa73e8
Lawrence <jiangyi0425@gmail.com>
2015-08-08 19:17:19 +0800

    Cpp

--- Premium/One-Edit-Distance.cpp
+++ Premium/One-Edit-Distance.cpp
@@ -1,6 +1,26 @@
 class Solution {
 public:
     bool isOneEditDistance(string s, string t) {
-        
+        if (abs((int)s.size() - (int)t.size()) > 1) return false;
+
+        if (t.size() == s.size()) {
+            for (int i = 0; i < t.size(); i++) {
+                if (s[i] != t[i]) {
+                    return (s.substr(i + 1) == t.substr(i + 1));
+                }
+            }
+            return false;
+        } else {
+            string& shortest = (s.size() < t.size() ? s : t);
+            string& longest = (s.size() > t.size() ? s : t);
+
+            for (int i = 0; i < shortest.size(); i++) {
+                if (shortest[i] != longest[i]) {
+                    return (shortest.substr(i) == longest.substr(i + 1));
+                }
+            }
+            return true;
+        }
     }
 };
+
