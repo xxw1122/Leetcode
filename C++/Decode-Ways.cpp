30c86ab0ad166f7d6c0588d6575e135e1b42ff02
Lawrence <jiangyi0425@gmail.com>
2015-08-08 23:05:17 +0800

    Cpp

--- C++/Decode-Ways.cpp
+++ C++/Decode-Ways.cpp
@@ -5,15 +5,16 @@ public:
         if (len == 0) return 0;
         vector<int> dp(len + 1, 0);
         dp[0] = 1;
-        if (s[0] != '0') dp[1] = 1;
-        for (int i = 2; i <= len; i ++) {
-            int cnt = (s[i - 2] - '0') * 10 + s[i - 1] - '0';
-            if (s[i - 1] == '0' && cnt <= 26 && cnt > 0) {
-                dp[i] = dp[i - 2];
-            } else if(s[i - 1] != '0' && s[i - 2] != '0' && cnt <= 26 && cnt > 0) {
-                dp[i] = dp[i - 1] + dp[i - 2];
-            } else if (s[i - 1] != '0') {
-                dp[i] = dp[i - 1];
+        for (int i = 1; i <= len; i ++) {
+            if (s[i - 1] == '0') {
+                if (i >= 2 && s[i - 2] < '3' && s[i - 2] != '0') {
+                    dp[i] += dp[i - 2];
+                }
+            } else {
+                dp[i] += dp[i - 1];
+                if (i >= 2 && s[i - 2] != '0' && ((s[i - 2] - '0') * 10 + s[i - 1] - '0') <= 26) {
+                    dp[i] += dp[i - 2];
+                }
             }
         }
         return dp[len];
