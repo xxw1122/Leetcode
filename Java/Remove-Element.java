9bae908c14637febb817032f07dc04d65e6e79c6
Lawrence <jiangyi0425@gmail.com>
2015-08-09 11:05:32 +0800

    Java

---
+++ Java/Remove-Element.java
@@ -0,0 +1,11 @@
+public class Solution {
+    public int removeElement(int[] nums, int val) {
+        int cur = 0;
+        for (int i = 0; i < nums.length; i ++) {
+            if (nums[i] != val) {
+                nums[cur ++] = nums[i];
+            }
+        }
+        return cur;
+    }
+}
